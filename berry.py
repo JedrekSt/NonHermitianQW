95a6fee1089d02c0d8f5aa7288ec7aaa4b421521
JedrekSt <JedrekSt>
2024-09-17 10:35:38 +0200

    non_unitary file

--- berry.py
+++ berry.py
@@ -16,10 +16,11 @@ class BerryPhase:
         ans_m =  np.abs(np.angle(math.prod(np.diag(Mat_m,k=1))*Mat_m[-1,0]))
         ans_p =  np.abs(np.angle(math.prod(np.diag(Mat_p,k=1))*Mat_p[-1,0]))
         return ans_m,ans_p
-    
+
     def vector_representation(self,model):
         _,Ut = eig(model.momentum_U())
         Ut = np.hstack(tuple(Ut[2*i : 2*i+2,2*i : 2*i+2] for i in range(self.dim)))
         Ut = Ut / np.sqrt(np.diag(op.dag(Ut)@Ut))
         ans = lambda ax: np.vstack(tuple( np.diag(op.dag(Ut[:,ax::2]) @ S_ @ Ut[:,ax::2]) for S_ in op.S))
         return ans(0),ans(1)
+    
