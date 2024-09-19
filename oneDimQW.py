ed1df6e5aad309242e37e45beb1765cf929c7093
JedrekSt <JedrekSt>
2024-09-19 16:06:42 +0200

    changes

--- oneDimQW.py
+++ oneDimQW.py
@@ -6,7 +6,7 @@ from scipy.linalg import expm
 class one_dim_QW:
     def __init__(self,dim,coin_dict,**kwargs):
         self.dim = dim
-        self.Evolution(**coin_dict)
+        self.U_ = self.Evolution(**coin_dict)
         self.state = self.initial_state(**kwargs)
 
     def Evolution(self,**kwargs):
@@ -14,11 +14,11 @@ class one_dim_QW:
         th = kwargs.get('th',np.pi/2)
         self.C_ = expm(- 1j * sum(n[i]*op.S[i] for i in range(len(n))) * th / 2)
         self.S_ = self.Step()
-        self.U_ = self.S_ @ np.kron(np.eye(self.dim),self.C_)
+        return self.S_ @ np.kron(np.eye(self.dim),self.C_)
     
     def initial_state(self,**kwargs):
         st = np.zeros(self.dim)
-        st[kwargs.get('x0',1)] = 1
+        st[kwargs.get('x0',self.dim//2)] = 1
         cst = np.array([kwargs.get('a',1),kwargs.get('b',1)])
         cst = cst / np.sqrt( op.dag(cst) @ cst )
         return np.kron(st,cst)
@@ -52,7 +52,7 @@ class ss_one_dim_QW(one_dim_QW):
         self.C2 = expm(- 1j * sum(n_ * S_  for n_,S_ in zip(n,op.S) ) * self.th2 / 2)
         self.Sr = np.kron(np.eye(self.dim),op.s_m) + np.kron(op.circ_shift(self.dim,k=1),op.s_p)
         self.Sl = np.kron(np.eye(self.dim),op.s_p) + np.kron(op.circ_shift(self.dim,k=-1),op.s_m)
-        self.U_ = self.Sr @ np.kron(np.eye(self.dim),self.C1) @ self.Sl @ np.kron(np.eye(self.dim),self.C2)
+        return self.Sr @ np.kron(np.eye(self.dim),self.C1) @ self.Sl @ np.kron(np.eye(self.dim),self.C2)
 
     def momentum_U(self):
         T = np.diag(np.exp(-1j*2*np.pi/self.dim * (np.arange(0,self.dim))))
@@ -76,7 +76,7 @@ class nonUnitary_QW(ss_one_dim_QW):
         self.Sl = np.kron(np.eye(self.dim),op.s_p) + np.kron(op.circ_shift(self.dim,k=-1),op.s_m)
         self.G_ = np.kron(np.eye(self.dim),expm(del_ * op.sz))
         self.G_p = np.kron(np.eye(self.dim),expm(-del_ * op.sz))
-        self.U_ = self.Sr @ self.G_ @ np.kron(np.eye(self.dim),self.C1) @ self.Sl @ self.G_p @ np.kron(np.eye(self.dim),self.C2)
+        return self.Sr @ self.G_ @ np.kron(np.eye(self.dim),self.C1) @ self.Sl @ self.G_p @ np.kron(np.eye(self.dim),self.C2)
 
     def momentum_U(self):
         T = np.diag(np.exp(-1j*2*np.pi/self.dim * (np.arange(0,self.dim)-self.dim//2)))
@@ -84,4 +84,24 @@ class nonUnitary_QW(ss_one_dim_QW):
         left = np.kron(np.eye(self.dim),op.s_p) + np.kron(op.dag(T),op.s_m)
         return right @ self.G_ @ np.kron(np.eye(self.dim),self.C1) @ left @ self.G_p @ np.kron(np.eye(self.dim),self.C2)
 
+class time_one_dim_QW(one_dim_QW):
 
+    def Evolution(self,**kwargs):
+        n = kwargs.get('n',[0,1,0])
+        def inner(th):
+            self.C_ = expm(- 1j * sum(n[i]*op.S[i] for i in range(len(n))) * th / 2)
+            self.S_ = self.Step()
+            return self.S_ @ np.kron(np.eye(self.dim),self.C_)
+        return inner
+    
+    def momentum_U(self,th):
+        T = np.diag(np.exp(-1j*2*np.pi/self.dim * (np.arange(0,self.dim)-self.dim//2)))
+        return (np.kron(T,op.s_p)+np.kron(op.dag(T),op.s_m)) @ np.kron(np.eye(self.dim),self.C_(th)) 
+    
+    def evolve(self,angles):
+        self.data = self.get_prob()
+        for i in range(len(angles)):
+            self.state = self.U_(angles[i]) @ self.state
+            self.data = np.vstack((self.data,self.get_prob()))
+        return self.data
+    
--- twoDimQW.py
+++ twoDimQW.py
@@ -1,13 +1,14 @@
 import numpy as np
 from operators import op
 from scipy.linalg import expm
+from numpy import pi
 
-class two_dim_QW:
+class single_coin_two_dim_QW:
     def __init__(self,dim_x,dim_y,x_coin_dict,y_coin_dict,**kwargs):
         self.dim_x = dim_x
         self.dim_y = dim_y
 
-        self.Evolution(x_coin_dict,y_coin_dict)
+        self.U_ = self.Evolution(x_coin_dict,y_coin_dict)
         self.state = self.initial_state(**kwargs)
 
     def Coin(self,**kwargs):
@@ -25,8 +26,9 @@ class two_dim_QW:
         Idx = np.eye(self.dim_x)
         Idy = np.eye(self.dim_y)
 
-        self.U_ = self.Sx @ op.n_fold_kron([Idx,Idy,self.Cx]) 
-        self.U_ = self.U_ @ self.Sy @ op.n_fold_kron([Idx,Idy,self.Cy]) 
+        U_ = self.Sx @ op.n_fold_kron([Idx,Idy,self.Cx]) 
+        U_ = U_ @ self.Sy @ op.n_fold_kron([Idx,Idy,self.Cy]) 
+        return U_
     
     def Step(self,**kwargs):
         if kwargs.get('axis','y')=='x':
@@ -59,7 +61,7 @@ class two_dim_QW:
             self.data.append(self.get_prob().reshape(self.dim_x,self.dim_y))
         return np.stack(self.data,axis=0)
     
-class diagonal_two_dim_QW(two_dim_QW):
+class diagonal_two_dim_QW(single_coin_two_dim_QW):
     def Evolution(self,x_coin_dict,y_coin_dict):
         self.Cx = self.Coin(x_coin_dict['th'],x_coin_dict['n'])
         self.Cy = self.Coin(y_coin_dict['th'],y_coin_dict['n'])
@@ -71,7 +73,86 @@ class diagonal_two_dim_QW(two_dim_QW):
         Idy = np.eye(self.dim_y)
         Id2 = np.eye(2)
 
-        self.U_ = self.Sy @ op.n_fold_kron([Idx,Idy,self.Cy]) 
-        self.U_ = self.U_ @ self.Sx @ op.n_fold_kron([Idx,Idy,self.Cx]) 
-        self.U_ = self.Sx @ self.Sy @ op.n_fold_kron([Idx,Idy,self.Cx]) @ self.U_  
+        U_ = self.Sy @ op.n_fold_kron([Idx,Idy,self.Cy]) 
+        U_ = U_ @ self.Sx @ op.n_fold_kron([Idx,Idy,self.Cx]) 
+        U_ = self.Sx @ self.Sy @ op.n_fold_kron([Idx,Idy,self.Cx]) @ U_  
+
+        return U_
         
+
+class double_coin_two_dim_QW:
+    def __init__(self,x_dim,y_dim,x_coin_dict,y_coin_dict,**kwargs):
+        self.x_dim = x_dim
+        self.y_dim = y_dim
+
+        self.Idx = np.eye(self.x_dim)
+        self.Idy = np.eye(self.y_dim)
+        self.Id2 = np.eye(2)
+
+        self.U_ = self.Evolution(x_coin_dict,y_coin_dict)
+        self.U_k = self.momentum_U()
+        self.psi = self.initial_state(**kwargs)
+
+    def Evolution(self,x_coin_dict,y_coin_dict):
+        self.Cx = self.Coin(**x_coin_dict)
+        self.Cy = self.Coin(**y_coin_dict)
+
+        self.S_ = self.Step()
+
+        return self.S_ @ op.n_fold_kron([self.Idx,self.Idy,self.Cx,np.eye(2)]) @ op.n_fold_kron([self.Idx,self.Idy,np.eye(2),self.Cy])
+    
+    def Coin(self,**kwargs):
+        n_ = kwargs.get('n',[0,1,0])
+        th = kwargs.get('th',pi/8)
+        return expm(-1j*sum(n_[i] * op.S[i] for i in range(len(n_)))*th/2)
+    
+    def Step(self):
+        Sx = op.n_fold_kron([op.circ_shift(self.x_dim,k=1),self.Idy,op.s_p,self.Id2])
+        Sx += op.n_fold_kron([op.circ_shift(self.x_dim,k=-1),self.Idy,op.s_m,self.Id2])
+
+        Sy = op.n_fold_kron([self.Idx,op.circ_shift(self.y_dim,k=1),self.Id2,op.s_p])
+        Sy += op.n_fold_kron([self.Idx,op.circ_shift(self.y_dim,k=-1),self.Id2,op.s_m])
+
+        return Sx @ Sy
+    
+    def initial_state(self,**kwargs):
+        x0 = kwargs.get("x0",self.x_dim//2)
+        y0 = kwargs.get("y0",self.y_dim//2)
+        ax = kwargs.get("ax",1)
+        bx = kwargs.get("bx",1j)
+        ay = kwargs.get("ay",1)
+        by = kwargs.get("by",1j)
+        psix = np.eye(1,self.x_dim,k=x0)[0]
+        psiy = np.eye(1,self.y_dim,k=y0)[0]
+        c_x = np.array([ax,bx])
+        c_y = np.array([ay,by])
+
+        psi = op.n_fold_kron([psix,psiy,c_x,c_y])
+        return psi / np.sqrt(op.dag(psi) @ psi)
+    
+    def get_Prob(self):
+        return (np.abs(sum(self.psi[i::4] for i in range(4)))**2).reshape(self.x_dim,self.y_dim)
+    
+    def Evolve(self,steps):
+        data = [self.get_Prob()]
+        for _ in range(steps):
+            self.psi = self.U_ @ self.psi
+            data.append(self.get_Prob())
+        return np.stack(data,axis=0)
+    
+    def momentum_U(self):
+        Tx = np.diag(np.exp(-1j* 2*pi/self.x_dim * np.arange(0,self.x_dim)))
+        Ty = np.diag(np.exp(-1j* 2*pi/self.y_dim * np.arange(0,self.y_dim)))
+
+        Step_x = op.n_fold_kron([Tx,self.Idy,op.s_p,np.eye(2)]) + op.n_fold_kron([op.dag(Tx),self.Idy,op.s_m,np.eye(2)])
+        Step_y = op.n_fold_kron([self.Idx,Ty,np.eye(2),op.s_p]) + op.n_fold_kron([self.Idx,op.dag(Ty),np.eye(2),op.s_m])
+
+        return Step_x @ Step_y @ op.n_fold_kron([self.Idx,self.Idy,self.Cx,np.eye(2)]) @ op.n_fold_kron([self.Idx,self.Idy,np.eye(2),self.Cy])
+
+
+
+
+    
+
+    
+
